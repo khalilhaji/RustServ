872741c7cf1d17454521a2c23d0c9b58726ee6a6
Khalil Haji <khalilalihaji@gmail.com>
2018-02-24 12:36:28 -0500

    receiving request now works

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::net::TcpListener;
+use std::net::TcpStream;
+use std::io::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("127.0.0.1:1337").unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+        read_request(stream);
+    }
+}
+
+
+fn read_request(mut stream: TcpStream) {
+    let mut buffer = [0; 512];
+
+    stream.read(&mut buffer).unwrap();
+
+    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
 }
