db6c36b8e5148edea1eea10496851ead3e5ebcbf
Khalil Haji <khalilalihaji@gmail.com>
2018-03-22 22:09:03 -0400

    Can now serve html

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,35 @@
+use std::io::prelude::*;
 use std::net::TcpListener;
 use std::net::TcpStream;
-use std::io::prelude::*;
+use std::fs::File;
+
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:1337").unwrap();
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-        read_request(stream);
+        println!("Connection established");
+
+        connection_handler(stream);
     }
 }
 
-
-fn read_request(mut stream: TcpStream) {
+fn connection_handler(mut stream: TcpStream) {
     let mut buffer = [0; 512];
 
     stream.read(&mut buffer).unwrap();
 
     println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+
+    let mut file = File::open("index.html").unwrap();
+
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).unwrap();
+
+    let response = format!("HTTP/1.1 200 OK\r\n\r\n{}", contents);
+
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
+
 }
